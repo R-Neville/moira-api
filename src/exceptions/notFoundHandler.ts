66a7ea8f54533481ed7985ec696a26ca3528782d
Robbie Neville <robertneville27@gmail.com>
2023-01-23 10:19:02 +1000

    Implement 404 handler

--- src/exceptions/notFoundHandler.ts
+++ src/exceptions/notFoundHandler.ts
@@ -1,16 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 import MoiraError from "./MoiraError";
+import logger from "../logger";
 
 export default function notFoundHandler(
   req: Request,
   _res: Response,
   next: NextFunction
 ) {
-  next(
-    new MoiraError({
-      title: "Not Found",
-      detail: `No resource could be found at ${req.method} ${req.originalUrl}`,
-      httpStatus: 404,
-    })
-  );
+  const error = new MoiraError({
+    title: "Not Found",
+    detail: `No resource could be found at ${req.method} ${req.originalUrl}`,
+    httpStatus: 404,
+  });
+  logger.error(error);
+  next(error);
 }
