b5cb4a507bf1e40e503611688ad96d526542a0e6
Robbie Neville <robertneville27@gmail.com>
2023-01-23 10:20:44 +1000

    Include logging in makeSchemaValidator

--- src/middleware/makeSchemaValidator.ts
+++ src/middleware/makeSchemaValidator.ts
@@ -1,11 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { Schema } from "joi";
 import { IMoiraResponse, respond } from "../utils";
+import logger from "../logger";
 
 export default function makeSchemaValidator(schema: Schema) {
   return (req: Request, res: Response, next: NextFunction) => {
     const { error } = schema.validate(req.body);
     if (error) {
+      logger.error(error);
       const { details } = error;
       const message = details.map((item) => item.message).join(",");
       respond(res, {
