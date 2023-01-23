f606ba485fad24dfe05c020f5c048b419dfefc7d
Robbie Neville <robertneville27@gmail.com>
2023-01-23 10:16:29 +1000

    Add users routes to API router index

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -2,9 +2,11 @@ import express, { Request, Response } from "express";
 import makeSchemaValidator from "../../middleware/makeSchemaValidator";
 import registrationSchema from "./validation-schemas/registration.schema";
 import loginSchema from "./validation-schemas/login.schema";
-import registerController from "./register.controller";
-import loginController from "./login.controller";
-import projectsRouter from "./projects";
+import registerController from "./auth/register.controller";
+import loginController from "./auth/login.controller";
+import projectsRouter from "./projectsRouter";
+import authorizeRequest from "./authorizeRequest";
+import usersRouter from "./usersRouter";
 
 const v1Router = express.Router();
 
@@ -14,8 +16,18 @@ v1Router.get("/", (req: Request, res: Response) => {
   });
 });
 
-v1Router.post("/register", makeSchemaValidator(registrationSchema), registerController);
+// Auth routes
+v1Router.post(
+  "/register",
+  makeSchemaValidator(registrationSchema),
+  registerController
+);
 v1Router.post("/login", makeSchemaValidator(loginSchema), loginController);
-v1Router.use("/projects", projectsRouter);
+
+// Users routes
+v1Router.use("/users", authorizeRequest(), usersRouter);
+
+// Projects routes
+v1Router.use("/projects", authorizeRequest(), projectsRouter);
 
 export default v1Router;
